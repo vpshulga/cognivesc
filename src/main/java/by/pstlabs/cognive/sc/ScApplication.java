bee79ef3e4f8abbab8883a45e15b9bc8ff187429
vpshulga <vpshulgaa@gmail.com>
2019-10-16 10:51:07 +0300

    Init commit

---
+++ src/main/java/by/pstlabs/cognive/sc/ScApplication.java
@@ -0,0 +1,13 @@
+package by.pstlabs.cognive.sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScApplication.class, args);
+	}
+
+}
